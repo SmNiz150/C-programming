6798a0a1e4411e913b8b0060a7c5707fc067dd16
SmNiz150 <106352707+SmNiz150@users.noreply.github.com>
2024-03-25 17:44:12 +0530

    Add files via upload

---
+++ palandrome.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+   char a[50];
+   int i, flag=0;
+   printf("\nEnter a number or word: ");
+   scanf("%s",a);
+   for(i=0; i<((strlen(a))/2); i++) {
+     if (a[i]!=a[strlen(a)-1-i])
+    {
+       printf("\nNot a palandrome");
+       flag=1;	
+       break;
+       }
+  
+   }
+  if(flag=0) {printf("\nPalandrome");}
+}
